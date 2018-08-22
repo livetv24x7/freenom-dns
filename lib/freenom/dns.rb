6041984270ee31fc16e5530906bb76ac1c005804
Thong Nguyen <thong.nguyencao.vn@gmail.com>
2018-08-22 14:03:30 +0700

    First commit

---
+++ lib/freenom/dns.rb
@@ -0,0 +1,7 @@
+require "freenom/dns/version"
+
+module Freenom
+  module Dns
+    # Your code goes here...
+  end
+end
---
+++ spec/freenom/dns_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Freenom::Dns do
+  it "has a version number" do
+    expect(Freenom::Dns::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
